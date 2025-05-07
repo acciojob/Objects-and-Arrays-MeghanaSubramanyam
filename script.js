0f73257af5faba7ec46b998d307af8ed4cd1d729
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2025-05-07 22:08:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Write your code here
+const team = players;                  // Reference to original array
+const team1 = [...players];            // Shallow copy using spread
+const cap1 = { ...person };            // Shallow copy of object
 
+// Expose to global scope
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+
